233827977a8f380a0e417d0fb71507d0a814b05c
PANFACTORY <yutatanabe510@gmail.com>
2022-11-13 18:00:00 +0900

    ♻️ Nodeクラスのリファクタリング

--- src/cpp/Node.h
+++ src/cpp/Node.h
@@ -1,72 +1,50 @@
-//*****************************************************************************
-//Title		:src/cpp/Node.h
-//Author	:Tanabe Yuta
-//Date		:2019/10/26
-//Copyright	:(C)2019 TanabeYuta
-//*****************************************************************************
-
+/**
+ * @file node.h
+ * @author PANFACTORY
+ * @brief
+ * @version 0.1
+ * @date 2022-11-13
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 
 #pragma once
 #define _USE_MATH_DEFINES
 #include <cmath>
-#include <vector>
 
+namespace DelaunayPAN {
+template <class T>
+class Node {
+   public:
+    Node() {
+        this->x = T();
+        this->y = T();
+        this->isset = false;
+        this->isonboundary = false;
+    }
+    ~Node() {}
+    Node(T _x, T _y) {
+        this->x = _x;
+        this->y = _y;
+        this->isset = false;
+        this->isonboundary = false;
+    }
 
-namespace DelaunayPAN{
-	template<class T>
-	class Node
-	{
-public:
-		Node();
-		~Node();
-		Node(T _x, T _y);
+    T x, y;             // cordinate vallue of Node
+    bool isset;         // is set
+    bool isonboundary;  // is on boundary
 
-		T x, y;					//cordinate vallue of Node
-		bool isset;				//is set						
-		bool isonboundary;		//is on boundary
-
-		T distance(const Node<T>& _node);							//get distance between other node
-		T vecpro(const Node<T>& _node0, const Node<T>& _node1);		//get innerproduct
-		T innpro(const Node<T>& _node0, const Node<T>& _node1);		//get vectorproduct
-	};
-
-
-	template<class T>
-	Node<T>::Node(){
-		this->x = T();
-		this->y = T();
-		this->isset = false;
-		this->isonboundary = false;
-	}
-
-
-	template<class T>
-	Node<T>::~Node(){}
-
-
-	template<class T>
-	Node<T>::Node(T _x, T _y){
-		this->x = _x;
-		this->y = _y;
-		this->isset = false;
-		this->isonboundary = false;
-	}
-
-
-	template<class T>
-	T Node<T>::distance(const Node<T>& _node) {
-		return sqrt(pow(this->x - _node.x, 2.0) + pow(this->y - _node.y, 2.0));
-	}
-
-
-	template<class T>
-	T Node<T>::vecpro(const Node<T>& _node0, const Node<T>& _node1) {
-		return (_node0.x - this->x)*(_node1.y - this->y) - (_node0.y - this->y)*(_node1.x - this->x);
-	}
-
-
-	template<class T>
-	T Node<T>::innpro(const Node<T>& _node0, const Node<T>& _node1) {
-		return (_node0.x - this->x)*(_node1.x - this->x) + (_node1.y - this->y)*(_node0.y - this->y);
-	}
-}
+    T distance(const Node<T>& _node) const {
+        return sqrt(pow(this->x - _node.x, 2.0) + pow(this->y - _node.y, 2.0));
+    }
+    T vecpro(const Node<T>& _node0, const Node<T>& _node1) const {
+        return (_node0.x - this->x) * (_node1.y - this->y) -
+               (_node0.y - this->y) * (_node1.x - this->x);
+    }
+    T innpro(const Node<T>& _node0, const Node<T>& _node1) const {
+        return (_node0.x - this->x) * (_node1.x - this->x) +
+               (_node1.y - this->y) * (_node0.y - this->y);
+    }
+};
+}  // namespace DelaunayPAN
